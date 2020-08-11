d18df2c585d361fc2a4ac3dbb71808490365b12d
my_zh <my_zhu@outlook.com>
2020-08-11 15:54:38 +0800

    itest and try QRselection

--- QRselection.py
+++ QRselection.py
@@ -1,3 +1,25 @@
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
+#import statsmodels as sm
+qr = sm.QuantReg()
+
+
+quantiles = np.arange(.05, .96, .1)
+def fit_model(q):
+    res = qr.fit(q=q)
+    return [q, res.params[0], res.params[1] + res.conf_int().loc[0]]
+
+models = [fit_model(x) for x in quantiles]
+models = pd.DataFrame(models, columns=['q', 'a', 'b1', 'b2', 'lb1', 'ub1'])
+
+qr_ci = qr.conf_int().loc['y'].tolist()
+qr = dict(a = qr.params[0],
+           b1 = qr.params[1],
+           b2 = qr.params[2],
+           lb = qr_ci[0],
+           ub = qr_ci[1])
+
+print(models)
+print(qr)
+
 
