437cb4822e6ed9470ee7790f3b868172143de02a
my_zh <my_zhu@outlook.com>
2020-08-21 11:08:02 +0800

    small change

--- SampleCharacter.py
+++ SampleCharacter.py
@@ -5,14 +5,3 @@ class SampleCharacter:
         self.lim = []
 
 
-def con_s(gen_sample_point, length, n_sample):
-    for point in gen_sample_point:
-        sc = SampleCharacter(length, point)
-        for i in range(len(n_sample)):
-            xl = point[i] - length[i] / 2
-            xu = point[i] + length[i] / 2
-            xlim = []
-            xlim.append(xl)
-            xlim.append(xu)
-            sc.lim.append(xlim)
-    return
--- XLim.py
+++ XLim.py
@@ -1,4 +1,5 @@
 import SampleCharacter
+import numpy as np
 
 
 class XLim:
@@ -13,6 +14,26 @@ class XLim:
         self.del_num = 0
 
 
-def con_sample(con_list, Xtrain, gen_):
+def con_sample(con_list, X_train, gen_x_point, del_point, sample_list, n_sample, length):
+    x_min = np.amin(X_train, axis=0)
+    for index in range(len(n_sample)):
+        x_list = []
+        for i in range(n_sample[index]):
+            xl = XLim(0, x_min[index]+i*length[index] - length[index] / 2,
+                      x_min[index] + i * length[index] + length[index] / 2,
+                      x_min[index]+i*length[index])
+            x_list.append(xl)
+        con_list.append(x_list)
 
+def con_s(gen_sample_point, length, n_sample, sample_list):
+    for point in gen_sample_point:
+        sc = SampleCharacter.SampleCharacter(length, point)
+        for i in range(len(n_sample)):
+            xl = point[i] - length[i] / 2
+            xu = point[i] + length[i] / 2
+            xlim = []
+            xlim.append(xl)
+            xlim.append(xu)
+            sc.lim.append(xlim)
+        sample_list.append(sc)
     return
